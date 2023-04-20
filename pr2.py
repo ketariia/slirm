478094f461840645aed07bfd2cb3114a816b8ff8
ketariaa <ketariia@gmail.com>
2023-04-20 19:14:52 +0300

    google

--- pr2.py
+++ pr2.py
@@ -9,4 +9,15 @@ elif age < 50:
 elif age > 50:
     print('u r old')   
 else:
-    print('u must be an immortal being')   
+    print('u must be an immortal being')  
+
+"""
+Script for asking user's name and age.
+Depending on how old the user is, different response message is printed.
+Example Input:
+Name: [Name]
+Age: [Number greater than 18 and less than 50]
+Example Output:
+nice, middle-aged
+"""
+ 
