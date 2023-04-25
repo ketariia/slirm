dfa18e8d8afa44a8e1ea3cbc67216a8b1fb05063
ketariaa <ketariia@gmail.com>
2023-04-25 19:39:35 +0300

    gg

--- epytext.py
+++ epytext.py
@@ -1,9 +1,13 @@
-# @brief This function calculates the sum of the elements in list
-# @param numbers <list> a list of numbers
-# @return int the sum of the numbers
+
 def sum_list(numbers):
     # The actual implementation of the function
     total = 0
     for num in numbers:
         total = total + num
     return total
+
+'''
+@brief This function calculates the sum of the elements in list
+ @param numbers <list> a list of numbers
+ @return int the sum of the numbers
+'''
