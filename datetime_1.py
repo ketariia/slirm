42d87fdf1d9fd447d6c50972d801f68057a21304
ketariaa <ketariia@gmail.com>
2023-04-27 16:37:23 +0300

    а

--- datetime_1.py
+++ datetime_1.py
@@ -1,25 +1,28 @@
 class Department:
-    def __init__(self, name, faculty, head, room_number, building_number, phone_number, num_teachers):
+    def __init__(self, name, faculty, head, number, floor, phone_number, student_capacity):
         self.name = name
         self.faculty = faculty
         self.head = head
-        self.room_number = room_number
-        self.building_number = building_number
+        self.number = number
+        self.floor = floor
         self.phone_number = phone_number
-        self.num_teachers = num_teachers
+        self.student_capacity = student_capacity
         self.disciplines = []
 
     def add_discipline(self, discipline):
         self.disciplines.append(discipline)
 
+    def __str__(self):
+        return f"{self.name}, факультет: {self.faculty}, заведующий: {self.head}, номер кабинета: {self.number}, этаж: {self.floor}, телефон: {self.phone_number}, вместимость студентов: {self.student_capacity}"
+
 class Teacher:
-    def __init__(self, last_name, first_name, patronymic, department, birth_year, start_year, experience, position, gender, address, city, phone_number):
+    def __init__(self, last_name, first_name, patronymic, department, birth_year, employment_year, experience, position, gender, address, city, phone_number):
         self.last_name = last_name
         self.first_name = first_name
         self.patronymic = patronymic
         self.department = department
         self.birth_year = birth_year
-        self.start_year = start_year
+        self.employment_year = employment_year
         self.experience = experience
         self.position = position
         self.gender = gender
@@ -31,11 +34,23 @@ class Teacher:
     def add_discipline(self, discipline):
         self.disciplines.append(discipline)
 
-    def get_academic_performance(self, discipline, student):
-        for performance in discipline.academic_performances:
-            if performance.student == student:
-                return performance.grade
-        return None
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.patronymic}, {self.position}, {self.experience} лет опыта работы, телефон: {self.phone_number}, адрес: {self.address}, город: {self.city}"
+
+class Discipline:
+    def __init__(self, name, department, teacher, hours, exam_type):
+        self.name = name
+        self.department = department
+        self.teacher = teacher
+        self.hours = hours
+        self.exam_type = exam_type
+        self.academic_performances = []
+
+    def add_academic_performance(self, performance):
+        self.academic_performances.append(performance)
+
+    def __str__(self):
+        return f"{self.name}, отделение: {self.department.name}, преподаватель: {self.teacher.last_name} {self.teacher.first_name} {self.teacher.patronymic}, часы: {self.hours}, тип экзамена: {self.exam_type}"
 
 class Student:
     def __init__(self, last_name, first_name, patronymic, department, birth_year, gender, address, city, phone_number):
@@ -59,17 +74,8 @@ class Student:
                 return performance.grade
         return None
 
-class Discipline:
-    def __init__(self, name, department, teacher, hours, final_control_type):
-        self.name = name
-        self.department = department
-        self.teacher = teacher
-        self.hours = hours
-        self.final_control_type = final_control_type
-        self.academic_performances = []
-
-    def add_academic_performance(self, performance):
-        self.academic_performances.append(performance)
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.patronymic}, {self.birth_year} года рождения, телефон: {self.phone_number}, адрес: {self.address}, город: {self.city}"
 
 class AcademicPerformance:
     def __init__(self, teacher, discipline, student, grade):
@@ -77,15 +83,15 @@ class AcademicPerformance:
         self.discipline = discipline
         self.student = student
         self.grade = grade
-        teacher.add_discipline(discipline)
-        student.add_academic_performance(self)
-        discipline.add_academic_performance(self)
 
-department = Department("Кафедра математики", "Факультет естественных наук", "Барболина Екатерина Сергеевна", 101, 2, "+7 (999) 999-99-99", 10)
+    def __str__(self):
+        return f"{self.student.last_name} {self.student.first_name} {self.student.patronymic} - {self.discipline.name}: {self.grade}"
+
+department = Department("Кафедра математики", "Факультет естественных наук", "Александрович Иван Алексеев", 101, 2, "+7 (999) 999-99-99", 10)
 teacher1 = Teacher("Барболина", "Екатерина", "Сергеевна", department, 1980, 2005, 15, "старший преподаватель", "женский", "ул. Пушкина, д. 10, кв. 5", "Москва", "+7 (999) 888-88-88")
 discipline1 = Discipline("Математический анализ", department, teacher1, 120, "экзамен")
 
-student1 = Student("Барболина", "Екатерина", "Сергеевна", department, 2000, "женский", "ул. Ленина, д. 23, кв. 12", "Москва", "+7 (999) 777-77-77")
+student1 = Student("Александрович", "Иван", "Алексеев", department, 2000, "мужской", "ул. Ленина, д. 23, кв. 12", "Москва", "+7 (999) 777-77-77")
 performance1 = AcademicPerformance(teacher1, discipline1, student1, 5)
 
 department.add_discipline(discipline1)
@@ -93,4 +99,6 @@ teacher1.add_discipline(discipline1)
 student1.add_academic_performance(performance1)
 discipline1.add_academic_performance(performance1)
 
-print("Имя студента:", student1.last_name, student1.first_name, student1.patronymic)
+print(student1)
+for performance in student1.academic_performances:
+    print(performance)
