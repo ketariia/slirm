12d00cef24448da63d2984c7f93e6ba954d586c2
ketariaa <ketariia@gmail.com>
2023-04-30 22:17:45 +0300

    с

--- doc1.py
+++ doc1.py
@@ -1,65 +1,3 @@
-class Discipline:
-    """
-    Initializes a new instance of the Discipline class.
-
-    Args:
-        name (str): The name of the discipline.
-        teacher (Teacher): The teacher who teaches the discipline.
-    """
-    def __init__(self, name: str, teacher: Teacher) -> None:
-        self.name = name
-        self.teacher = teacher
-        self.academic_performances = []
-
-    def add_academic_performance(self, academic_performance: AcademicPerformance) -> None:
-        """
-        Adds an instance of the AcademicPerformance class to the list of academic performances for this discipline.
-
-        Args:
-            academic_performance (AcademicPerformance): The academic performance to be added.
-        """
-        self.academic_performances.append(academic_performance)
-
-
-class Department:
-    """
-    Initializes a new instance of the Department class.
-
-    Args:
-        name (str): The name of the department.
-    """
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.disciplines = []
-
-    def add_discipline(self, discipline: Discipline) -> None:
-        """
-        Adds an instance of the Discipline class to the list of disciplines for this department.
-
-        Args:
-            discipline (Discipline): The discipline to be added.
-        """
-        self.disciplines.append(discipline)
-
-class Teacher:
-    """
-    Initializes a new instance of the Teacher class.
-
-    Args:
-        name (str): The name of the teacher.
-    """
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.disciplines = []
-
-    def add_discipline(self, discipline: Discipline) -> None:
-        """
-        Adds an instance of the Discipline class to the list of disciplines this teacher teaches.
-
-        Args:
-            discipline (Discipline): The discipline to be added.
-        """
-        self.disciplines.append(discipline)
 class AcademicPerformance:
     """
     Initializes a new instance of the AcademicPerformance class.
@@ -69,7 +7,86 @@ class AcademicPerformance:
         student (Student): The student to whom this academic performance belongs.
         grade (float): The grade for this academic performance.
     """
-    def __init__(self, discipline: Discipline, student: Student, grade: float) -> None:
+    def __init__(self, discipline: "Discipline", student: "Student", grade: float) -> None:
         self.discipline = discipline
         self.student = student
         self.grade = grade
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the AcademicPerformance object.
+
+        Returns:
+            str: A string representation of the object.
+        """
+        return f"{self.student.name} received a {self.grade} in {self.discipline.name}"
+
+    def __add__(self, other: "AcademicPerformance") -> "AcademicPerformance":
+        """
+        Adds two AcademicPerformance objects together.
+
+        Args:
+            other (AcademicPerformance): The AcademicPerformance object to add.
+
+        Returns:
+            AcademicPerformance: An instance of AcademicPerformance with the sum of the grades.
+        """
+        new_grade = self.grade + other.grade
+        return AcademicPerformance(self.discipline, self.student, new_grade)
+
+    def __sub__(self, other: "AcademicPerformance") -> "AcademicPerformance":
+        """
+        Subtracts one AcademicPerformance object from another.
+
+        Args:
+            other (AcademicPerformance): The AcademicPerformance object to subtract.
+
+        Returns:
+            AcademicPerformance: An instance of AcademicPerformance with the difference of the grades.
+        """
+        new_grade = self.grade - other.grade
+        return AcademicPerformance(self.discipline, self.student, new_grade)
+
+    def __mul__(self, other: float) -> "AcademicPerformance":
+        """
+        Multiplies an AcademicPerformance object by a float.
+
+        Args:
+            other (float): The float to multiply by.
+
+        Returns:
+            AcademicPerformance: An instance of AcademicPerformance with the multiplied grade.
+        """
+        new_grade = self.grade * other
+        return AcademicPerformance(self.discipline, self.student, new_grade)
+
+    def __truediv__(self, other: float) -> "AcademicPerformance":
+        """
+        Divides an AcademicPerformance object by a float.
+
+        Args:
+            other (float): The float to divide by.
+
+        Returns:
+            AcademicPerformance: An instance of AcademicPerformance with the divided grade.
+        """
+        new_grade = self.grade / other
+        return AcademicPerformance(self.discipline, self.student, new_grade)
+ap1 = AcademicPerformance(discipline1, student1, 4.0)
+ap2 = AcademicPerformance(discipline1, student1, 3.5)
+
+# Add two AcademicPerformance objects
+ap3 = ap1 + ap2
+print(ap3.grade)  # Output: 7.5
+
+# Subtract one AcademicPerformance object from another
+ap4 = ap1 - ap2
+print(ap4.grade)  # Output: 0.5
+
+# Multiply an AcademicPerformance object by a float
+ap5 = ap1 * 2
+print(ap5.grade)  # Output: 8.0
+
+# Divide an AcademicPerformance object by a float
+ap6 = ap1 / 2
+print(ap6.grade)  # Output: 2.0
